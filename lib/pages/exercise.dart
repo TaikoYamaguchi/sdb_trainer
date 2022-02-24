ced4a359ae2e8f7882aa1ec80abe7ba148c96f5e
Taiko Yamaguchi <twin1235@gmail.com>
2022-02-24 21:50:15 +0900

    Workout list

--- lib/pages/exercise.dart
+++ lib/pages/exercise.dart
@@ -9,6 +9,48 @@ class Exercise extends StatefulWidget {
 }
 
 class _ExerciseState extends State<Exercise> {
+  List<Map<String, String>> datas = [];
+  double top = 0;
+  double bottom = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    datas = [
+      {
+        "workout": "가슴삼두",
+        "exercise": "벤치프레스",
+        "number": "1"
+      },
+      {
+        "workout": "어깨",
+        "exercise": "숄더프레스,밀리터리 프레스",
+        "number": "2"
+      },
+      {
+        "workout": "하체",
+        "exercise": "스쿼트,파워레그프레스,레그익스텐션",
+        "number": "3"
+      },
+    ];
+  }
+
+/*
+  switch (index) {
+    case 0:
+      return Home();
+      break;
+    case 1:
+      return Exercise();
+      break;
+    case 2:
+      return Calendar();
+      break;
+    case 3:
+      return Container();
+      break;
+  }
+*/
 
   PreferredSizeWidget _appbarWidget(){
     return AppBar(
@@ -29,18 +71,49 @@ class _ExerciseState extends State<Exercise> {
   }
 
   Widget _bodyWidget() {
-    return ListView.separated(
+    return Container(
+      color: Colors.black,
+      child: ListView.separated(
+        padding: EdgeInsets.symmetric(horizontal: 5),
         itemBuilder: (BuildContext _context, int index){
+          if(index==0){top = 20; bottom = 0;} else if (index==datas.length-1){top = 0;bottom = 20;} else {top = 0;bottom = 0;};
           return Container(
-            child: Text(
-              index.toString(),
+            child: Expanded(
+              child: Container(
+                padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+                decoration: BoxDecoration(
+                  color: Color(0xFF212121),
+                  borderRadius: BorderRadius.only(
+                  topRight: Radius.circular(top),
+                  bottomRight: Radius.circular(bottom),
+                  topLeft: Radius.circular(top),
+                  bottomLeft: Radius.circular(bottom)
+                  )
+                ),
+                height: 52,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      datas[index]["workout"].toString(),
+                      style: TextStyle(fontSize: 21, color: Colors.white),
+                    ),
+                    Text(
+                      "${datas[index]["number"].toString()} Exercises",
+                      style: TextStyle(fontSize: 13, color: Color(0xFF717171))
+                    )
+                  ],
+                ),
+              ),
             ),
           );
         },
         separatorBuilder: (BuildContext _context, int index){
           return Container(height:1, color: Color(0xFF717171),);
         },
-        itemCount: 3
+        itemCount: datas.length
+      ),
     );
   }
 
