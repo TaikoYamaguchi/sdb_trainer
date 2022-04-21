e6ba8912eec8e40835cb87f5f4e990fef782b8a8
Taiko Yamaguchi <twin1235@gmail.com>
2022-04-22 00:28:27 +0900

    search2

--- flutter/lib/pages/each_workout.dart
+++ flutter/lib/pages/each_workout.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:provider/provider.dart';
 import 'package:sdb_trainer/pages/each_exercise.dart';
+import 'package:sdb_trainer/pages/exercise.dart';
+import 'package:sdb_trainer/providers/exercisesdata.dart';
 import 'package:transition/transition.dart';
 
 
@@ -15,6 +18,7 @@ class EachWorkoutDetails extends StatefulWidget {
 }
 
 class _EachWorkoutDetailsState extends State<EachWorkoutDetails> {
+  var _exercisesdataProvider;
   List<Map<String, dynamic>> datas = [];
   double top = 0;
   double bottom = 0;
@@ -141,7 +145,7 @@ class _EachWorkoutDetailsState extends State<EachWorkoutDetails> {
       child: Column(
         children: [
           Container(
-            margin: const EdgeInsets.fromLTRB(10, 10, 10, 10),
+            margin: const EdgeInsets.fromLTRB(10, 16, 10, 16),
             child: TextField(
               style: TextStyle(color: Colors.white),
               decoration: InputDecoration(
@@ -155,84 +159,8 @@ class _EachWorkoutDetailsState extends State<EachWorkoutDetails> {
               ),
             ),
           ),
-          Expanded(
-            child: ListView.separated(
-                padding: EdgeInsets.symmetric(horizontal: 5),
-                itemBuilder: (BuildContext _context, int index){
-                  final exinfo = widget.uniqueinfo.where((unique){
-                    final info = unique.name;
-                    return info.contains(widget.exerciselist[index].name);
-                  }).toList();
-                  if(index==0){top = 20; bottom = 0;} else if (index==widget.exerciselist.length-1){top = 0;bottom = 20;} else {top = 0;bottom = 0;};
-                  return GestureDetector(
-                    onTap: () {
-                      Navigator.push(context,Transition(
-                          child: EachExerciseDetails(
-                            exercisedetail: widget.exerciselist[index],
-                            eachuniqueinfo: exinfo,
-                          ),
-                          transitionEffect: TransitionEffect.RIGHT_TO_LEFT
-                      ));
-                    },
-                    child: Container(
-                      child: Container(
-                        padding: EdgeInsets.symmetric(horizontal: 20),
-                        decoration: BoxDecoration(
-                            color: Color(0xFF212121),
-                            borderRadius: BorderRadius.only(
-                                topRight: Radius.circular(top),
-                                bottomRight: Radius.circular(bottom),
-                                topLeft: Radius.circular(top),
-                                bottomLeft: Radius.circular(bottom)
-                            )
-                        ),
-                        height: 52,
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              widget.exerciselist[index].name,
-                              style: TextStyle(fontSize: 21, color: Colors.white),
-                            ),
-
-                            Container(
-                              child: Row(
-                                //mainAxisAlignment: MainAxisAlignment.spaceAround,
-                                children: [
-                                  Text(
-                                      "Rest: ${widget.exerciselist[index].rest}",
-                                      style: TextStyle(fontSize: 13, color: Color(0xFF717171))
-                                  ),
-                                  Expanded(child: SizedBox()),
-                                  Text(
-                                      "1RM: ${exinfo[0].onerm}/${exinfo[0].goal}unit",
-                                      style: TextStyle(fontSize: 13, color: Color(0xFF717171))
-                                  ),
-                                ],
-                              ),
-                            )
-                          ],
-                        ),
-                      ),
-                    ),
-                  );
-                },
-                separatorBuilder: (BuildContext _context, int index){
-                  return Container(
-                    alignment: Alignment.center,
-                    height:1, color: Color(0xFF212121),
-                    child: Container(
-                      alignment: Alignment.center,
-                      margin: EdgeInsets.symmetric(horizontal: 10),
-                      height:1, color: Color(0xFF717171),
-                    ),
-                  );
-
-                },
-                itemCount: widget.exerciselist.length
-            ),
-          ),
+          ExerciseState.exercisesWidget(
+              _exercisesdataProvider.exercisesdata, true)
         ],
       ),
     );
@@ -240,6 +168,8 @@ class _EachWorkoutDetailsState extends State<EachWorkoutDetails> {
 
   @override
   Widget build(BuildContext context) {
+    _exercisesdataProvider =
+        Provider.of<ExercisesdataProvider>(context, listen: false);
     return Scaffold(
       appBar: _appbarWidget(),
       body: _isexsearch
