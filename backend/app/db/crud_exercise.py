33bd19ca2ab96940dc7f4c429f5ae118b5de5ec6
leechanwook <cksdnr1@gmail.com>
2022-04-28 22:09:13 +0900

    profile goal perfect

--- backend/app/db/crud_exercise.py
+++ backend/app/db/crud_exercise.py
@@ -23,3 +23,20 @@ def create_exercise(db: Session, exercise: schemas.ExercisesCreate):
 def get_exercise_by_email(db: Session, email: str) -> schemas.ExercisesOut:
     exercise = db.query(models.Exercises).filter(models.Exercises.user_email == email).first()
     return exercise
+
+def edit_exercise(db:Session, exercise:schemas.ExercisesCreate):
+    db_exercise = get_exercise_by_email(db, exercise.user_email)
+    if not db_exercise:
+        raise HTTPException(status.HTTP_404_NOT_FOUND, detail="User not found")
+    update_exercise = exercise.dict(exclude_unset=True)
+
+    for key,value in update_exercise.items():
+        setattr(db_exercise, key, value)
+
+    db.add(db_exercise)
+    db.commit()
+    db.refresh(db_exercise)
+    return db_exercise
+
+    
+
