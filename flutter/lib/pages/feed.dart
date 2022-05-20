8293bfbf78aa3d8e91c531efc91e8806303be23a
leechanwook <cksdnr1@gmail.com>
2022-05-20 21:52:12 +0900

    feed list

--- flutter/lib/pages/feed.dart
+++ flutter/lib/pages/feed.dart
@@ -17,24 +17,158 @@ class Feed extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    _historydata = Provider.of<HistorydataProvider>(context);
+    _historydata = Provider.of<HistorydataProvider>(context, listen: false);
     _historydata.getdata();
     print("111111");
-    print(_historydata.historydata.sdbdatas[0].user_email);
+    print(_historydata.historydata.sdbdatas[0].exercises[0]);
     return Scaffold(
         appBar: AppBar(
             title: Text("피드", style: TextStyle(color: Colors.white)),
             backgroundColor: Colors.black),
-        body: Container(child: _feedCard(context)));
+        body: _feedCardList(context));
   }
 
-  Widget _feedCard(context) {
-    return Card(
-        child: Column(children: [
-      Text(_historydata.historydata.sdbdatas[0].user_email),
-      Text(_historydata.historydata.sdbdatas[0].exercises[0].date),
-      Text(_historydata.historydata.sdbdatas[0].exercises[0].name),
-      Text(_historydata.historydata.sdbdatas[0].exercises[1].name),
-    ]));
+  Widget _feedCardList(context) {
+    return ListView.separated(
+        itemBuilder: (BuildContext _context, int index) {
+          return Center(
+              child: _feedCard(_historydata.historydata.sdbdatas[index]));
+        },
+        separatorBuilder: (BuildContext _context, int index) {
+          return Container(
+            alignment: Alignment.center,
+            height: 1,
+            color: Colors.black,
+            child: Container(
+              alignment: Alignment.center,
+              height: 1,
+              color: Color(0xFF717171),
+            ),
+          );
+        },
+        itemCount: _historydata.historydata.sdbdatas.length);
+  }
+
+  Widget _feedCard(SDBdata) {
+    return Container(
+      height: 200,
+      color: Colors.black,
+      child: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Card(
+            color: Color(0xFF717171),
+            child: Column(
+              children: [
+                Padding(
+                    padding:
+                        const EdgeInsets.symmetric(horizontal: 20, vertical: 0),
+                    child: Row(
+                      children: [
+                        Text(
+                          SDBdata.user_email,
+                          style: TextStyle(fontSize: 18, color: Colors.white),
+                        ),
+                      ],
+                    )),
+                Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 20, vertical: 0),
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Container(
+                          width: 120,
+                          child: Text(
+                            "운동",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 18,
+                            ),
+                          )),
+                      Container(
+                          width: 70,
+                          child: Text("sets",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 18,
+                              ),
+                              textAlign: TextAlign.center)),
+                      Container(
+                          width: 80,
+                          child: Text("1rm",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 18,
+                              ),
+                              textAlign: TextAlign.center))
+                    ],
+                  ),
+                ),
+                Expanded(
+                  flex: 10,
+                  child: ListView.separated(
+                      itemBuilder: (BuildContext _context, int index) {
+                        return Center(
+                            child: _exerciseWidget(
+                                SDBdata.exercises[index], index));
+                      },
+                      separatorBuilder: (BuildContext _context, int index) {
+                        return Container(
+                          alignment: Alignment.center,
+                          height: 1,
+                          color: Colors.black,
+                          child: Container(
+                            alignment: Alignment.center,
+                            height: 1,
+                            color: Color(0xFF717171),
+                          ),
+                        );
+                      },
+                      itemCount: SDBdata.exercises.length),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _exerciseWidget(Exercises, index) {
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Container(
+              width: 120,
+              child: Text(
+                Exercises.name,
+                style: TextStyle(fontSize: 18, color: Colors.white),
+              ),
+            ),
+            Container(
+              width: 70,
+              child: Text(
+                Exercises.sets.length.toString(),
+                style: TextStyle(fontSize: 18, color: Colors.white),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            Container(
+              width: 80,
+              child: Text(
+                Exercises.onerm.toString(),
+                style: TextStyle(fontSize: 18, color: Colors.white),
+                textAlign: TextAlign.center,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
