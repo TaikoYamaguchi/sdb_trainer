d9027779c4fab3d562fe6193ee79c1258902939e
Taiko Yamaguchi <twin1235@gmail.com>
2022-12-17 15:26:44 +0900

    get onpressd

--- flutter/lib/src/utils/alerts.dart
+++ flutter/lib/src/utils/alerts.dart
@@ -154,6 +154,23 @@ Future<dynamic> showUpdateVersion(_appUpdateVersion, context) {
 }
 
 Future<dynamic> showsimpleAlerts(layer ,rindex, eindex, context) {
+  // layer 1: delete, 2: add ex cancel
+  String title= '';
+  String subtitle= '';
+  String comment= '';
+  switch (layer){
+    case 1:
+      title = "루틴을 지울 수 있어요";
+      subtitle = '정말로 루틴을 지우시나요?';
+      comment = '루틴을 지우면 복구 할 수 없어요';
+      break;
+    case 2:
+      title = "편집이 저장되지 않아요";
+      subtitle = '루틴 편집을 종료하시겠나요?';
+      comment = '오른쪽 위를 클릭하면 저장할 수 있어요';
+      break;
+  };
+
   return showDialog(
       context: context,
       builder: (context) {
@@ -162,28 +179,28 @@ Future<dynamic> showsimpleAlerts(layer ,rindex, eindex, context) {
             borderRadius: BorderRadius.circular(8.0),
           ),
           backgroundColor: Theme.of(context).cardColor,
-          title: Text('루틴을 지울 수 있어요',
+          title: Text(title,
               textAlign: TextAlign.center,
               style: TextStyle(color: Colors.white, fontSize: 24)),
           content: Column(
             mainAxisSize: MainAxisSize.min,
             children: [
-              Text('정말로 루틴을 지우시나요?',
+              Text(subtitle,
                   textAlign: TextAlign.center,
                   style: TextStyle(color: Colors.white, fontSize: 16)),
-              Text('루틴을 지우면 복구 할 수 없어요',
+              Text(comment,
                   textAlign: TextAlign.center,
                   style: TextStyle(color: Colors.grey, fontSize: 12)),
             ],
           ),
           actions: <Widget>[
-            _DeleteConfirmButton(rindex, context),
+            _DeleteConfirmButton_r(rindex, context),
           ],
         );
       });
 }
 
-Widget _DeleteConfirmButton(rindex, context) {
+Widget _DeleteConfirmButton_r (rindex, context) {
   var _workoutProvider = Provider.of<WorkoutdataProvider>(context, listen: false);
   var _userProvider = Provider.of<UserdataProvider>(context, listen: false);
   void _editWorkoutCheck() async {
@@ -218,6 +235,37 @@ Widget _DeleteConfirmButton(rindex, context) {
             Navigator.of(context, rootNavigator: true).pop();
           },
           child: Text("삭제",
+              style: TextStyle(fontSize: 20.0, color: Colors.white))));
+}
+
+Widget _ExsearchOutButton(context) {
+  var _workoutProvider = Provider.of<WorkoutdataProvider>(context, listen: false);
+  var _userProvider = Provider.of<UserdataProvider>(context, listen: false);
+  return SizedBox(
+      width: MediaQuery.of(context).size.width,
+      child: TextButton(
+          style: TextButton.styleFrom(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(8.0),
+            ),
+            backgroundColor: Theme.of(context).primaryColor,
+            textStyle: TextStyle(
+              color: Colors.white,
+            ),
+            disabledForegroundColor: Color.fromRGBO(246, 58, 64, 20),
+            padding: EdgeInsets.all(12.0),
+          ),
+          onPressed: () {
+            //_workoutProvider.changebudata(widget.rindex);
+            //_workoutNameCtrl.clear();
+            //_exSearchCtrl.clear();
+            //searchExercise(_exSearchCtrl.text);
+            //setState(() {
+            //  _isexsearch = !_isexsearch;
+            //});
+            Navigator.of(context, rootNavigator: true).pop();
+          },
+          child: Text("편집 취소 하기",
               style: TextStyle(fontSize: 20.0, color: Colors.white))));
 }
 
