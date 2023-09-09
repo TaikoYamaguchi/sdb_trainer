9c62c9ce49986fc02f33cfe1ea7e865fb6ecd67f
Chanwook Lee <cksdnr1@gmail.com>
2023-09-09 23:05:59 +0900

    기록 에서 날짜한 운동 누르면 밑에 운동정보변함

--- flutter/lib/providers/chartIndexState.dart
+++ flutter/lib/providers/chartIndexState.dart
@@ -3,9 +3,11 @@ import 'package:flutter/cupertino.dart';
 class ChartIndexProvider extends ChangeNotifier {
   int _chartIndex = 0;
   int _staticIndex = 0;
+  int _staticChartPointIndex = -1;
   var _isPageController = PageController(initialPage: 0);
   int get chartIndex => _chartIndex;
   int get staticIndex => _staticIndex;
+  int get staticChartPointIndex => _staticChartPointIndex;
 
   PageController get isPageController => _isPageController;
 
@@ -17,6 +19,20 @@ class ChartIndexProvider extends ChangeNotifier {
   changeStaticIndex(state) {
     _staticIndex = state;
     notifyListeners();
+  }
+
+  changeStaticChartPointIndex(state) {
+    WidgetsBinding.instance!.addPostFrameCallback((_) {
+      _staticChartPointIndex = state;
+      notifyListeners();
+    });
+  }
+
+  initStaticChartPointIndex(state) {
+    WidgetsBinding.instance!.addPostFrameCallback((_) {
+      _staticChartPointIndex = -1;
+      notifyListeners();
+    });
   }
 
   changePageController(state) {
